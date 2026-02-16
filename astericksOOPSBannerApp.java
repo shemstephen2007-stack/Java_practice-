f52a7d3de61568fb771b01cef7a93ee6802602a3
Shem Stephen <shemstephen2007@gmail.com>
2026-02-16 14:01:50 +0530

    Add files via upload

---
+++ astericksOOPSBannerApp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class astericksOOPSBannerApp
+{
+	public static void main(String args[])
+	{
+		System.out.println("   ***   " +"      ***   "+"      ******  "+"     *****");
+		System.out.println(" **   **   "+"  **   **   "+"   **    **  "+"   **");
+		System.out.println("**     **   "+"**      **   "+" **     ** "+" **");
+		System.out.println("**     **   "+"**      **   "+" **    ** "+"   **");
+		System.out.println("**     **   "+"**      **   "+" ****** "+"       ***");
+		System.out.println("**     **   "+"**      **   "+" ** "+"              **");
+		System.out.println("**     **   "+"**      **   "+" ** "+"               **");
+		System.out.println(" **    **   "+" **    **   "+"  ** "+"              **");
+		System.out.println("   ***   "+"      ***   "+"     ** "+"         *****");
+	}
+}
+		
+		
+		
+		
