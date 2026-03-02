bae3766ad9466c27137eb158f511eb1e69daedbf
Shem Stephen <shemstephen2007@gmail.com>
2026-03-02 13:05:35 +0530

    Add files via upload

---
+++ joinastericks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class joinastericks
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println(String.join(" ","   ***   " ,"      ***   ","      ******  ","     *****"));
+		System.out.println(String.join(" "," **   **   ","  **   **   ","   **    **  ","  **"));
+		System.out.println(String.join(" ","**     **   ","**      **   "," **     ** "," **"));
+		System.out.println(String.join(" ","**     **   ","**      **   "," ****** ","     **"));
+		System.out.println(String.join(" ","**     **   ","**      **   "," ** ","            ***"));
+		System.out.println(String.join(" ","**     **   ","**      **   "," ** ","              **"));
+		System.out.println(String.join(" ","**     **   ","**      **   "," ** ","               **"));
+		System.out.println(String.join(" "," **   **   ","  **    **   ","  ** ","              **"));
+		System.out.println(String.join(" ","   ***   ","      ***   ","     ** ","          *****"));
+	}
+}
