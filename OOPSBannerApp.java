6351a203c67c03b2279a199a774190d7e41094b1
Shem Stephen <shemstephen2007@gmail.com>
2026-03-02 15:46:13 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/*
+*OOPSBannerApp UC1- OOPS BANNER DISPLAY APPLICATION
+*
+*This class demonstrates a simple java program that displays the Object oriented programming system (or) OOPS acronym to the console
+*
+*@author developer
+* @version 1.0
+*/
+
+
+import java.util.*;
+public class OOPSBannerApp
+{
+	public static void main(String args[])
+	{
+		System.out.println("OOPS");
+	}
+}
